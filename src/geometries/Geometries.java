83ec446633dad819e9ff271c681f6124c0d17c64
zoahr97 <zoahr97@gmail.com>
2024-06-02 14:08:47 +0300

    Stage 3- Final

--- src/geometries/Geometries.java
+++ src/geometries/Geometries.java
@@ -6,6 +6,7 @@ import primitives.Ray;
 import java.util.*;
 
 public class Geometries  implements Intersectable{
+    /*
    private final List<Intersectable> intersectables=new LinkedList<Intersectable>();
 
     public Geometries() {
@@ -52,3 +53,33 @@ public class Geometries  implements Intersectable{
     }
 
 }
+*/
+
+    private final List<Intersectable> Geometry = new LinkedList<>();
+
+    Geometries(){}
+
+    public Geometries(Intersectable... geometries){
+        add(geometries);
+    }
+
+    public void add(Intersectable... geometries){
+        Geometry.addAll(List.of(geometries));
+
+    }
+
+    public List<Point> findIntersections(Ray ray){
+        List<Point> Intersection = null;
+        for (Intersectable geometry : Geometry) {
+            List<Point>  i = (geometry.findIntersections(ray));
+            if(i!=null){
+                if (Intersection==null){
+                    Intersection= new ArrayList<>();
+                }
+                Intersection.addAll(i);
+            }
+        }
+        return Intersection;
+    }
+}
+
--- unittests/geometries/GeometriesTest.java
+++ unittests/geometries/GeometriesTest.java
@@ -13,11 +13,11 @@ import static org.junit.jupiter.api.Assertions.*;
 /**
  *
  */
+
 class GeometriesTest {
 
-
+/*
     @Test
-
     void testFindIntersections() {
         Sphere sphere = new Sphere(new Point(0, 0, 5), 3); // ספירה שנמצאת מרכזית במרחק 5 מהמקור
         Plane plane = new Plane(new Point(0, 0, 0), new Vector(0, 0, 1)); // מישור הנמצא בגובה 0 מהמקור, מקביל לציר ה־Z
@@ -48,7 +48,39 @@ class GeometriesTest {
 //        // TC05: all geometries intersect with the ray
 assertEquals(3,geometries.findIntersections(new Ray(new Point(-3,0,0), new Vector(0, 0, -1))),
                "Expected three intersection");
-
-
     }
+    */
+    @Test
+    void testFindIntersections(){
+        Geometries Geomet=new Geometries();
+
+        Geomet.add(new Plane(new Point(0,0,1),new Point(0,1,0),new Point(1,0,0)));
+        Geomet.add(new Cylinder(1,new Ray(new Point(1,1,1),new Vector(1,1,1)),2));
+        Geomet.add(new Sphere(new Point(1,0,0),1));
+        Geomet.add(new Triangle(new Point(1,0,0),new Point(0,1,0),new Point(0,0,1)));
+        Geomet.add(new Tube(1,new Ray(new Point(0,0,1),new Vector(0,0,1))));
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: some of the geometries intersect with the ray
+        assertEquals(3,Geomet.findIntersections(new Ray(new Point(0,0,0),new Vector(1,1,1))).size(),"more then one intsersections");
+
+        // =============== Boundary Values Tests ==================
+        // TC02: no geometry intersect with the ray
+        assertNull(Geomet.findIntersections(new Ray(new Point(10,10,10),new Vector(0,0,1))), "Ray does not intersect with any geometry");
+
+        // TC03: only one geometry intersect with the ray
+        assertEquals(1,Geomet.findIntersections(new Ray(new Point(0,0,0),new Vector(0,0,1))).size(),"one intsersections");
+
+        // TC04: all geometries intersect with the ray
+        assertEquals(4,Geomet.findIntersections(new Ray(new Point(-2,-2,-2),new Vector(1,1,1))).size(),"all intsersections");
+
+        // TC05: empty list
+        assertNull(Geomet.findIntersections(new Ray(new Point(10,10,10),new Vector(0,0,1))), "empty list");
+    }
+
 }
+
+
+
+
+
+
